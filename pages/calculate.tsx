0b3f2ebfebe4d4dc3b4c1069ee80937d2a4edeb1
bentaylor <ben10taylor10@hotmail.com>
2021-02-28 16:58:34 +0000

    added form validation + loader gif

--- pages/calculate.tsx
+++ pages/calculate.tsx
@@ -3,23 +3,25 @@ import styles from "../styles/Calculate.module.css"
 import Head from 'next/head'
 
 const Calculate: React.FC = () => {
-
+    const [loader, setLoader] = useState<boolean>(false)
     // holds api return value
     const [savings, setSavings] = useState<boolean|number>(false)
     // conditional value to display result when data is fetched
     const [showResult, setShowResult] = useState<boolean>(false)
     // holds form data
     const [formData, setFormData] = useState({currentCost: 0, employeeCount: 0})
-
     // sets form data
     const setFormValue = (e) => {
         setFormData(prev => ({...prev, [e.target.name]: e.target.value}))
     }
-
     // fetches data from api
     const fetchSavings = async (e) => {
+        // check fields have been filled correctly
+        if(formData.currentCost > 0 && formData.employeeCount > 0){
        // prevent form refresh
        e.preventDefault()
+       // set loader
+       setLoader(true)
        // fetch data
        const result = await fetch("https://ember-challenge.vercel.app/api/calculate", {
         method: "POST",
@@ -28,10 +30,15 @@ const Calculate: React.FC = () => {
           },
         body: JSON.stringify(formData)
        }).then(res => res.json())
+       console.log(result)
        // set data, show result and clear form fields
        setSavings(result.savings)
        setShowResult(true)
        setFormData({currentCost: 0, employeeCount: 0})
+       setLoader(false)
+        } else{
+            alert("Please fill all fields correctly.")
+        }
     }
 
     return (
@@ -65,6 +72,7 @@ const Calculate: React.FC = () => {
                 </form>
             </div>
             <div className={styles.calculate__output}>
+                {loader && <img className={styles.loader} src="./loader.gif" alt="Loading animation."/>}
                 {showResult && (
                     <>
                     <p className={styles.calculate__text}>You'll save</p>
